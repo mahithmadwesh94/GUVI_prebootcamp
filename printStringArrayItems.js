bc194334b8dbbd347734a51965e530a4ea9dbcd4
mahith.madwesh <45761582+mahithmadwesh94@users.noreply.github.com>
2021-01-13 13:02:04 +0530

    Create printStringArrayItems.js

---
+++ printStringArrayItems.js
@@ -0,0 +1,13 @@
+//Iterate though string array and print the elements
+
+    var strArray= ["<option>Jazz</option>",
+    ,"<option>Blues</option>",
+ ,"<option>New Age</option>",
+ ,"<option>Classical</option>",
+ ,"<option>Opera</option>"]
+
+
+ for(let i=1;i<strArray.length;i++){
+     if(strArray[i])
+    console.log(strArray[i]);
+ }
