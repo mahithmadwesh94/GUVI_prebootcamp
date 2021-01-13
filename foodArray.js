c947222beac47cec89b324ecbb2e1c64b0bc6f1f
mahith.madwesh <45761582+mahithmadwesh94@users.noreply.github.com>
2021-01-13 13:14:20 +0530

    Create foodArray.js

---
+++ foodArray.js
@@ -0,0 +1,7 @@
+//Create an array called foods holds the names of your top 20 favorite foods, starting with the best food.
+
+let foods = [
+    'Dosa','Sambhar','Idli','Medu Vadai','Upma','Rasam','Appam','MYSORE MASALA DOSA','PARUPPU VADA',
+    'AVIYAL','GUNTA PONGANALU',' SWEET PONGAL','ONION PAKODA','LEMON RICE','COCONUT CHUTNEY','TOMATO CHUTNEY',
+    'SPICY POTATO FRY','PANNER BUTTER MASAL','CURD RICE','PAPAD'
+]
