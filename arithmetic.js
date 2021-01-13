784d62b974dc87faa58ef0fd0d303b7ffa1d8457
mahith.madwesh <45761582+mahithmadwesh94@users.noreply.github.com>
2021-01-13 12:33:23 +0530

    Create arithmetic.js

---
+++ arithmetic.js
@@ -0,0 +1,23 @@
+//Given two numbers and perform all arithmetic operations.
+
+function arithmeticOperations(num1,num2){
+  
+  console.log('Product: '+num1*num2);
+
+  console.log('Add: ',num1+num2);
+
+  console.log('Subtract: ',num1-num2);
+
+  console.log('Divie: ',num1/num2);
+
+  console.log('Modulus: ',num1%num2);
+
+  console.log('Pre Increment: ',++num1);
+
+  console.log('Post Increment: ',num1++);
+
+  console.log('Pre Decrement: ',--num2);
+
+  console.log('Post Decrement: ',num2--);
+   
+}
