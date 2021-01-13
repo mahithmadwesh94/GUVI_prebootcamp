c3474db8f8155d43311551e9574d0a92fd7ddbd5
mahith.madwesh <45761582+mahithmadwesh94@users.noreply.github.com>
2021-01-13 10:21:25 +0530

    Update Task_1.3.js

--- Task_1.3.js
+++ Task_1.3.js
@@ -1,18 +1,6 @@
 //Declare variables to store your first name, last name, marital status, country and age in multiple lines
 
-// Getting input via STDIN
-const readline = require("readline");
-const userInput = [];
-const inp = readline.createInterface({
-  input: process.stdin
-});
 
-inp.on("line", (data) => {
-  userInput.push(data);
-});
-
-
-inp.on("close", () => {
 
 let firstName;
 let lastName;
@@ -20,5 +8,3 @@ let maritalStatus;
 let country;
 let age;
  
- 
-});
