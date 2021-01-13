94dee92f735c32fde339972445ef4e8632977c75
mahith.madwesh <45761582+mahithmadwesh94@users.noreply.github.com>
2021-01-13 10:22:44 +0530

    Update Task_2.1.js

--- Task_2.1.js
+++ Task_2.1.js
@@ -1,21 +1,6 @@
 //Square of a number
 
 
-// Getting input via STDIN
-const readline = require("readline");
-const userInput = [];
-const inp = readline.createInterface({
-  input: process.stdin
-});
-
-inp.on("line", (data) => {
-  userInput.push(data);
-});
-
-
-inp.on("close", () => {
-
-
 
 function returnSquare(num){
     return num*num;
@@ -25,7 +10,7 @@ console.log(returnSquare(2));// logs 4
 console.log(returnSquare(-1));// logs 1
 
 
-});
+
 
 
 
