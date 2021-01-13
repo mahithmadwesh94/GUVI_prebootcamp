dc03d2c5392533495cd6261d457ef192d50eb788
mahith.madwesh <45761582+mahithmadwesh94@users.noreply.github.com>
2021-01-13 13:16:56 +0530

    Create foodArrayLength.js

---
+++ foodArrayLength.js
@@ -0,0 +1,11 @@
+//Foods variable holds the names of your top 20 favorite foods, starting with the best food. How can you find your fifth favorite food?
+let foods = [
+    'Dosa','Sambhar','Idli','Medu Vadai','Upma','Rasam','Appam','MYSORE MASALA DOSA','PARUPPU VADA',
+    'AVIYAL','GUNTA PONGANALU',' SWEET PONGAL','ONION PAKODA','LEMON RICE','COCONUT CHUTNEY',
+    'TOMATO CHUTNEY','SPICY POTATO FRY','PANNER BUTTER MASAL','CURD RICE','PAPAD'
+];
+
+//Fifth favorite food
+console.log(foods[4]);//logs Upma
+
+console.log(foods.length);// logs 20
