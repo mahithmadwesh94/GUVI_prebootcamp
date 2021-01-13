5a26610abdfe3b4b7cfe3cd6756de4fb0648a343
mahith.madwesh <45761582+mahithmadwesh94@users.noreply.github.com>
2021-01-13 13:05:05 +0530

    Create arrayLength.js

---
+++ arrayLength.js
@@ -0,0 +1,11 @@
+//write a code to count the elements in the array . Don’t use length property
+
+function arrayLength(arr){
+    let sum = 0
+
+    arr.forEach((e)=>{
+        sum++;
+    })
+    return sum;
+}
+ 
