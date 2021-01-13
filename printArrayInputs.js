8a6134cb4bffccdff3dd0662f19236f48cc0e7c0
mahith.madwesh <45761582+mahithmadwesh94@users.noreply.github.com>
2021-01-13 14:40:50 +0530

    Create printArrayInputs.js

---
+++ printArrayInputs.js
@@ -0,0 +1,18 @@
+//Print the contents of the input variable
+
+var input = [
+    ["0001", "Roman Alamsyah", "Bandar Lampung", "21/05/1989", "Membaca"],
+    ["0002", "Dika Sembiring", "Medan", "10/10/1992", "Bermain Gitar"],
+    ["0003", "Winona", "Ambon", "25/12/1965", "Memasak"],
+    ["0004", "Bintang Senjaya", "Martapura", "6/4/1970", "Berkebun"]
+    ]
+
+
+    function dataHandling(input){
+        for (var i = 0; i < input.length; i++) {
+       console.log(input[i])
+        }
+        }
+        
+        dataHandling(input);
+ 
