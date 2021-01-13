3ddf36649b0362b06ff1c819001b51d05f2aac6d
mahith.madwesh <45761582+mahithmadwesh94@users.noreply.github.com>
2021-01-13 12:26:13 +0530

    Create circleAttributes.js

---
+++ circleAttributes.js
@@ -0,0 +1,14 @@
+//
+
+
+function circleProperties(r){
+  
+    let circumference = 2*Math.PI*r;
+
+    let diameter = 2*r;
+
+    let area = Math.PI*(r*r);
+
+    return {circumference: circumference,diameter:diameter,area:area};
+ 
+}
