808b317c62e1facd76d51dd1686f597349d0e1d2
mahith.madwesh <45761582+mahithmadwesh94@users.noreply.github.com>
2021-01-13 10:21:07 +0530

    Update Task_1.2.js

--- Task_1.2.js
+++ Task_1.2.js
@@ -1,23 +1,10 @@
 //How to get value of the variable myvar as output
 
-// Getting input via STDIN
-const readline = require("readline");
-const userInput = [];
-const inp = readline.createInterface({
-  input: process.stdin
-});
-
-inp.on("line", (data) => {
-  userInput.push(data);
-});
-
-
-inp.on("close", () => {
  
 var myvar= 1;
 console.log(myvar);// logs 1
  
  
-});
+
 
 
